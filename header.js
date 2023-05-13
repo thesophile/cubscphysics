13910b1fe136a9bcb0065318ae7ff5e2f3e68f00
Sophile <abhinavpb249@gmail.com>
2023-05-13 16:05:59 +0530

    Add files via upload

---
+++ header.js
@@ -0,0 +1,19 @@
+
+
+let appHeader = `    
+    <h1>Bsc Physics Calicut University </h1>
+	<h3>Study materials </h3>
+	
+	<nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="Textbooks.html">Books</a></li>                
+                <li><a href="experiments.html">Experiments</a></li>
+                <li><a href="classes.html">Classes</a></li>
+                <li><a href="contact.html">About </a></li>
+            </ul>
+        </nav>
+`;
+
+document.getElementById("app-header").innerHTML = appHeader;
+
