ef210ac94d7317b950cd7ea340ad8935d429bb76
Sophile <abhinavpb249@gmail.com>
2023-05-15 20:37:44 +0530

    Add files via upload

--- header.js
+++ header.js
@@ -6,14 +6,33 @@ let appHeader = `
 	
 	<nav>
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="Textbooks.html">Books</a></li>                
-                <li><a href="experiments.html">Experiments</a></li>
-                <li><a href="classes.html">Classes</a></li>
-                <li><a href="contact.html">About </a></li>
+                <li> <div  id ="index" onclick='goto(this.id)'>     Home</div></li>
+                <li> <div id="Textbooks" onclick='goto(this.id)'>   Books</div></li>                
+                <li> <div id="experiments" onclick='goto(this.id)'> Experiments</div></li>
+                <li> <div id="classes" onclick='goto(this.id)'>     Classes</div></li>
+                <li> <div id="about" onclick='goto(this.id)'>       About </div></li>
             </ul>
         </nav>
 `;
 
 document.getElementById("app-header").innerHTML = appHeader;
+
+
+window.onload=function Highlight(){
+//get the name of the current page
+var path = window.location.pathname;
+var page = path.split("/").pop();
+var name = page.split(".")[0];
+console.log( name );
+//get the button with that name
+var Button = document.getElementById(name);
+//change that color of the button
+Button.style.backgroundColor="lightgrey";
+}
+
+
+ function goto(buttonID){
+        file_name =buttonID+".html"	
+	window.open(file_name,"_self")
+}
 
