bf9d2134e3bf8411e3dde12e0d7a98f2d6c71d5f
abhinavpb-249 <abhinavpb249@gmail.com>
2023-06-30 21:55:36 +0530

    update

--- header.js
+++ header.js
@@ -32,22 +32,29 @@ Button.style.border="3px solid black";
 
 
 
-
+//To go to the page when the link from the menu is clicked
  function goto(buttonID){
         file_name =buttonID+".html"	
 	window.open(file_name,"_self")
 }
 
-  const hamburgerIcon = document.getElementById('hamburger-icon');
-  const menu = document.getElementsByClassName('menu')[0];
+
+//toggle display of menu
+
+$("#hamburger-icon").click(function(){
+  $(".menu").toggle();
+});
+
+// const hamburgerIcon = document.getElementById('hamburger-icon');
+// const menu = document.getElementsByClassName('menu')[0];
 
  
-hamburgerIcon.addEventListener('click', function() {
-  const computedStyle = getComputedStyle(menu);
-  if (computedStyle.display === "none") {
-    menu.style.display = "block";
-  } else {
-    menu.style.display = "none";
-  }
-});
+// hamburgerIcon.addEventListener('click', function() {
+//   const computedStyle = getComputedStyle(menu);
+//   if (computedStyle.display === "none") {
+//     menu.style.display = "block";
+//   } else {
+//     menu.style.display = "none";
+//   }
+// });
 
