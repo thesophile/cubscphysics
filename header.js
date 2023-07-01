3e9c6f1c15699515796dcc1ac5ad742e5b82ce4d
abhinavpb-249 <abhinavpb249@gmail.com>
2023-07-01 12:01:38 +0530

    test

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 
-// setheader();
+setheader();
 
-// function setheader(){
+function setheader(){
 let appHeader = `    
 
 	<header>
@@ -22,27 +22,8 @@ let appHeader = `
 
 document.getElementById("app-header").innerHTML = appHeader;
 
-// }
-
-// get the name of the current page
-getpage();
-function getpage(){
-var path = window.location.pathname;
-var page = path.split("/").pop();
-var name = page.split(".")[0];
-console.log( name );
-//get the button with that name
-var Button = document.getElementById(name);
-//change that color of the button
-Button.style.border="3px solid black";
 }
 
-
-// To go to the page when the link from the menu is clicked
-function goto(buttonID){
-        file_name =buttonID+".html"	
-	window.open(file_name,"_self")
-}
 
 
 
