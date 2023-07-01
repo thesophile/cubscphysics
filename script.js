65380b5bf48e32f9fbb5b4770980768080433f99
abhinavpb-249 <abhinavpb249@gmail.com>
2023-07-01 10:52:46 +0530

    test

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// get the name of the current page
+var path = window.location.pathname;
+var page = path.split("/").pop();
+var name = page.split(".")[0];
+console.log( name );
+//get the button with that name
+var Button = document.getElementById(name);
+//change that color of the button
+Button.style.border="3px solid black";
 
+
+
+// To go to the page when the link from the menu is clicked
+function goto(buttonID){
+        file_name =buttonID+".html"	
+	window.open(file_name,"_self")
+}
+
+
+// toggle display of menu
+document.getElementById("hamburger-icon").onclick = myFunction;
+	function myFunction() {
+	  		$(".menu").toggle();	
+	}
 
 
